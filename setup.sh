cdbed8021e0a8d060f2c36ed7d3313a1a713e28e
Øyvind Hovdsveen <oyvind.hovdsveen@gmail.com>
2017-09-15 21:25:06 +0200

    WIP setup

--- setup.sh
+++ setup.sh
@@ -2,7 +2,13 @@
 #adduser oyho sudo
 #git clone https://github.com/oyhovd/dotfiles.git
 
-sudo apt-get install zsh vim
+cd $HOME
+sudo apt-get install zsh vim ctags
+
+#zsh setup
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+ln -s $HOME/dotfiles/oyho.zsh-theme $HOME/.oh-my-zsh/themes/
+ln -s /Users/hovdsveen/dotfiles/debian_common.zshrc $HOME/.zshrc
 
 #vim setup
 ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
@@ -10,3 +16,7 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 #Run PluginInstall in vim
 vim +PluginInstall +qall
+
+#utils
+mkdir -p $HOME/bin
+ln -s $HOME/dotfiles/utils/* $HOME/bin/
