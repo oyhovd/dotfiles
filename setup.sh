fe20304fb2b81c24aef9502e3f23adbe5704a2d8
Øyvind Hovdsveen <oyvind.hovdsveen@gmail.com>
2017-09-15 21:38:07 +0200

    WIP setup

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 #git clone https://github.com/oyhovd/dotfiles.git
 
 cd $HOME
+#install dependencies necessary for the installers in this file
 sudo apt-get install zsh vim ctags curl
 
 #zsh setup
@@ -16,8 +17,12 @@ ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 #Run PluginInstall in vim
+vim +PluginClean +qall
 vim +PluginInstall +qall
 
 #utils
 mkdir -p $HOME/bin
 ln -s $HOME/dotfiles/utils/* $HOME/bin/
+
+#git setup
+git config --global core.excludesfile $HOME/dotfiles/gitignore_global
