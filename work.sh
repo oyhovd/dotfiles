b6b4e45cd28a29f8fd8ec1b7f555e135b88de85d
oyho <oyvind.hovdsveen@nordicsemi.no>
2017-11-09 10:32:20 +0100

    Adding work.sh

---
+++ work.sh
@@ -0,0 +1,4 @@
+#install zephyr dependencies. See zephyr home page for details.
+sudo apt-get install --no-install-recommends git cmake ninja-build gperf \
+  ccache doxygen dfu-util device-tree-compiler \
+  python3-ply python3-pip python3-setuptools xz-utils file
