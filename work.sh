d0309d09dc7dfed826ce667ccde507c699d4256d
Øyvind Hovdsveen <oyvind.hovdsveen@gmail.com>
2017-11-09 20:05:47 +0100

    x

--- work.sh
+++ work.sh
@@ -1,10 +1,10 @@
 #install zephyr dependencies as listed on web page.. See zephyr home page for details.
-sudo apt-get install --no-install-recommends git cmake ninja-build gperf \
+sudo apt-get install -y --no-install-recommends git cmake ninja-build gperf \
   ccache doxygen dfu-util device-tree-compiler \
   python3-ply python3-pip python3-setuptools xz-utils file
 
 
 #additional dependencies for zephyr projects
-sudo apt-get install libncurses5-dev
+sudo apt-get install -y libncurses5-dev
 
 echo "Make sure to run \"pip3 install --user -r scripts/requirements.txt\" in the zephyr folder"
